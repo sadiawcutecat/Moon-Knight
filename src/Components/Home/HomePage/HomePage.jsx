2503c6aa24b090e74e1f98bec0d9c6e0f2fade89
sadiawcutecat <98077710+sadiawcutecat@users.noreply.github.com>
2023-10-09 10:43:34 +0600

    navbar section added

---
+++ src/Components/Home/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import Banner from "../Banner/Banner";
+import Navbar from "../Navbar/Navbar";
+
+
+const HomePage = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default HomePage;
---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from './Components/Home/HomePage/HomePage.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage></HomePage>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
